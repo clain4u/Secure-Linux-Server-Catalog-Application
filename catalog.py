2fb8f6347e4bbc9761e42d856834e815c653684e
Clain Dsilva <clain4u@gmail.com>
2019-02-23 15:31:12 +0530

    Display all categories in home page.

--- catalog.py
+++ catalog.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, url_for
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from database_setup import Base, Category, Products
 app = Flask(__name__)
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/restaurants/')
 def restaurantMenu():
-    return render_template('home.html')
+    category = session.query(Category).all()
+    items = session.query(Products).limit(2)
+    return render_template('home.html', category=category, items=items)
 
 
 if __name__ == '__main__':
