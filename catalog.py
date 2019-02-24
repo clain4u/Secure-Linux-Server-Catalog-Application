64ae87280e0af66db4004d86ec77b4d2140f1580
Clain Dsilva <clain4u@gmail.com>
2019-02-24 09:16:13 +0530

    add new poduct, list product by category.

--- catalog.py
+++ catalog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, jsonify
+from flask import Flask, render_template, url_for, jsonify, request
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from database_setup import Base, Category, Products
@@ -13,7 +13,7 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-@app.route('/categories/')
+@app.route('/catalog/')
 def categories():
     category = session.query(Category).all()
     # joining using ForeignKey relationship to get parent category
@@ -23,13 +23,37 @@ def categories():
     return render_template('home.html', category=category, items=items)
 
 
-@app.route('/categories/JSON')
+@app.route('/categories/JSON', methods=['GET'])
 def catgoriesJSON():
     category = session.query(Category).all()
-    items = session.query(Products).order_by("id desc")
     return jsonify(categories=[i.serialize for i in category])
+
+
+@app.route('/catalog/<string:category_name>/<int:category_id>/view/')
+def categoryView(category_name, category_id):
+    products = session.query(Products).filter_by(category_id=category_id).all()
+    return render_template('category.html', category=category_name,
+                           products=products)
+
+
+@app.route('/products/new/', methods=['GET', 'POST'])
+def newProduct():
+    if request.method == 'POST':
+        productName = request.form.get('txtName')
+        category = request.form.get('selCategory')
+        price = request.form.get('txtPrice')
+        description = request.form.get('txtDescription')
+        newProduct = Products(name=productName, price=price,
+                              description=description, category_id=category)
+        session.add(newProduct)
+        session.commit()
+        return "product added to database"
+    else:
+        categories = session.query(Category).all()
+        return render_template('_add-product.html', categories=categories)
 
 
 if __name__ == '__main__':
     app.debug = True
+    print "Catalog app running at port:5000 Url- http://localhost:5000/catalog"
     app.run(host='0.0.0.0', port=5000)
